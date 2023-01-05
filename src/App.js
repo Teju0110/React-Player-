4c59cd9397b774788e3710109937fc6b7f1d8189
tejasvisawant1998@gmail.com <tejasvisawant1998@gmail.com>
2023-01-05 12:00:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
+import video from './restaurant_web.mp4'
+import audio from './audio.mp3'
 
 function App() {
+
+  const videoRef=useRef()
+  const audioRef=useRef()
+
+  const handlePlay =()=>{
+    videoRef.current.play();
+    //audioRef.current.play();
+
+  }
+  const handlePause =()=>{
+    videoRef.current.pause();
+    //audioRef.current.pause();
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='buttons'>
+        <button className='btn' onClick={handlePlay} style={{backgroundColor:"blue"}}>
+          Play
+        </button>
+        <button className='btn' onClick={handlePause} style={{backgroundColor:"red"}}>
+          Pause
+        </button>
+      </div>
+      <video height="350px" width="650px" ref={videoRef} loop>
+        <source src={video} />
+      </video>
+      <audio controls ref={audioRef} style={{marginTop:"20px"}}>
+        <source src={audio}/>
+      </audio>
     </div>
   );
 }
